b28370931236c7e03e0f94506f27c4f8d145e917
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-11-09 13:32:52 -0300

    Atualiza configurações do Django no arquivo settings.py, incluindo ajustes nas credenciais do banco de dados e estruturação das seções INSTALLED_APPS, MIDDLEWARE e TEMPLATES.

--- backend/tere_verde/settings.py
+++ backend/tere_verde/settings.py
@@ -1,15 +1,65 @@
-DATABASES =  {
+
+
+
+DEBUG = True  
+
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tere_verde_db',
-        'USER': 'user',
-        'PASSWORD': '""',
-        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '123Jhonath@n', 
+        'HOST': '127.0.0.1',                         
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"
         }
-
- }
-
+    }
 }
+
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',  
+]
+
+# URL para servir arquivos estáticos em desenvolvimento
+STATIC_URL = '/static/'
+
+# Opcional: se quiser colocar uma pasta de estáticos do projeto (desenvolvimento)
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+# Para coleta de estáticos em produção, defina STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
