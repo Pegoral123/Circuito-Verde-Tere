38345577971e4b27715a4f4bdc01fdad3a8d94f5
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-11-09 12:02:21 -0300

    Adiciona classes Evento e Novidade ao arquivo models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -20,4 +20,20 @@ class Trilha(models.Model):
 
     def __str__(self):
         return f"{self.nome} ({self.parque.nome}) - {self.status} - {self.extensão_km} km - {self.dificuldade}"
-      
+    
+  
+class Evento(models.Model):
+    parque = models.ForeignKey(Parque, on_delete=models.SET_NULL, null=True, blank=True , related_name='eventos')
+    titulo = models.CharField(max_length=100)
+    data = models.DateField()
+    local = models.CharField(max_length=255)
+    descricao = models.TextField()   
+
+class Novidade(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data_publicacao = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
