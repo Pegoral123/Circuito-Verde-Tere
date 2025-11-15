006307194a9463b8b9622d7f445545ebdcbb8fab
Pegoral123 <jhonathanpegoral123@gmail.com>
2025-11-15 20:14:14 -0300

    feat: Adiciona CORS_ALLOWED_ORIGINS para permitir requisições de origens específicas

--- backend/tere_verde/settings.py
+++ backend/tere_verde/settings.py
@@ -62,7 +62,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",    
+    "http://localhost:8080",
 ]
 
 TEMPLATES = [
